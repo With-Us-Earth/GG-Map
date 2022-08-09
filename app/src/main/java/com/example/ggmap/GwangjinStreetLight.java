0d7b9b6501083061db46b1b6da56bc987e42bbf3
U_Zin <930yujin@gmail.com>
2022-08-10 00:49:28 +0900

    광진구 가로등 api

---
+++ app/src/main/java/com/example/ggmap/GwangjinStreetLight.java
@@ -0,0 +1,17 @@
+package com.example.ggmap;
+
+public class GwangjinStreetLight {
+    public String name;
+    public double latitude;
+    public double longitude;
+
+    // set function
+    public void setName(String name){ this.name = name; }
+    public void setLatitude(double latitude){ this.latitude = latitude; }
+    public void setLongitude(double longitude){ this.longitude = longitude; }
+
+    // get function
+    public String getName(){ return this.name; }
+    public double getLatitude(){ return this.latitude; }
+    public double getLongitude(){ return this.longitude; }
+}
